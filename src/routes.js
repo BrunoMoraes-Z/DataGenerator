f99eecb8f578534ec10c73325b8778e45d675209
Bruno Moraes <moraes.7bruno@gmail.com>
2021-01-23 01:09:25 -0300

    ceps reais

--- src/routes.js
+++ src/routes.js
@@ -6,18 +6,25 @@ const help = require('./controllers/HelpController');
 const pessoa = require('./controllers/PessoaController');
 const card = require('./controllers/CardController');
 
+const ceps = require('../ceps.json');
+
 const routes = express.Router();
 
+const amount = ceps.totalCeps;
+// routes.get('/', (request, response) => response.json(ceps.ceps[Math.floor(Math.random() * ceps.ceps.length)]));
 routes.get('/', help.new);
 
 routes.get('/pessoa', pessoa.new);
-routes.get('/email', (request, response) => response.json({email: api.pessoa.cpf() + '@mailinator.com'}));
-routes.get('/cpf', (request, response) => response.json({cpf: api.pessoa.cpf()}));
-routes.get('/rg', (request, response) => response.json({rg: api.pessoa.rg().replace('.', '').replace('.', '').replace('-', '')}));
-routes.get('/cep', (request, response) => response.json({cep: api.localizacao.cep()}));
+routes.get('/email', (request, response) => response.json({ email: api.pessoa.cpf() + '@mailinator.com' }));
+routes.get('/cpf', (request, response) => response.json({ cpf: api.pessoa.cpf() }));
+routes.get('/rg', (request, response) => response.json({ rg: api.pessoa.rg().replace('.', '').replace('.', '').replace('-', '') }));
+routes.get('/cep', (request, response) => response.json(ceps.ceps[Math.floor(Math.random() * ceps.ceps.length)]));
+// routes.get('/cep', (request, response) => response.json({ cep: api.localizacao.cep() }));
 routes.get('/on_creditcard', card.on_help);
 routes.get('/on_creditcard/:flag', card.on_new);
 routes.get('/off_creditcard', card.off_new);
-routes.get('/creditcardvalidate', card.validate)
+routes.get('/creditcardvalidate', card.validate);
+
+
 
 module.exports = routes;
